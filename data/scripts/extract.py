aaa4765b9cfd2239e8bb39b398c77a5b81ca3b6f
harrydalgleish <harry.dalgleish@sky.com>
2026-07-13 14:52:25 +0000

    Added new .ipynb files and amended extract.py to contain subroutines

--- data/scripts/extract.py
+++ data/scripts/extract.py
@@ -1,8 +1,21 @@
 import requests
 
-url = "https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/competitions.json"
+def download_competitions():
+    url = "https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/competitions.json"
 
-response = requests.get(url)
+    response = requests.get(url)
 
-with open("data/raw/competitions.json", "w") as file:
-    file.write(response.text)
+    with open("data/raw/competitions.json", "w") as file:
+        file.write(response.text)
+
+def download_matches():
+    competition_id = 2
+    season_id = 44
+
+    specific_url = f"https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/matches/{competition_id}/{season_id}.json"
+    specific_file = f"{competition_id}_{season_id}.json"
+
+    response = requests.get(specific_url)
+
+    with open(f"data/raw/matches/{specific_file}", "w") as file:
+        file.write(response.text)
