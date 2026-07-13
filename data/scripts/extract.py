71312f6c5627a7534e53f8066a11119fb41e334b
harrydalgleish <harry.dalgleish@sky.com>
2026-07-13 16:24:39 +0000

    Amended extract.py

--- data/scripts/extract.py
+++ data/scripts/extract.py
@@ -8,14 +8,20 @@ def download_competitions():
     with open("data/raw/competitions.json", "w") as file:
         file.write(response.text)
 
-def download_matches():
-    competition_id = 2
-    season_id = 44
-
+def download_matches(competition_id, season_id):
     specific_url = f"https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/matches/{competition_id}/{season_id}.json"
     specific_file = f"{competition_id}_{season_id}.json"
 
     response = requests.get(specific_url)
 
     with open(f"data/raw/matches/{specific_file}", "w") as file:
+        file.write(response.text)
+
+def download_events(match_id):
+    specific_url = f"https://raw.githubusercontent.com/statsbomb/open-data/refs/heads/master/data/events/{match_id}.json"
+    specific_file = f"{match_id}.json"
+
+    response = requests.get(specific_url)
+
+    with open(f"data/raw/events/{specific_file}", "w") as file:
         file.write(response.text)
