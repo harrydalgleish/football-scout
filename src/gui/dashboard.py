1424cb2d945c9ed67e0053a016c5d17337baa466
harrydalgleish <harry.dalgleish@sky.com>
2026-07-29 21:42:25 +0000

    Dashboard Creation

--- src/gui/dashboard.py
+++ src/gui/dashboard.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
-from playerview import PlayerView
+#from playerview import PlayerView
 from mplsoccer import Pitch
+from utils.data_loader import load_competitions
 
 class Dashboard:
-    def __init__(self, root)
+    def __init__(self, root):
         self.root = root
 
         self.create_sidebar()
@@ -11,6 +12,8 @@ class Dashboard:
         self.create_formation()
 
     def create_sidebar(self):
+        competitions = load_competitions()
+        competition_names = [comp["competition_name"] for comp in competitions]
         self.sidebar = ctk.CTkFrame(
             self.root,
             width = 400
@@ -19,21 +22,28 @@ class Dashboard:
             side="left",
             fill="y"
         )
-        title = ctk.CTkLabel(
+        self.title = ctk.CTkLabel(
             self.sidebar,
             text="Selection",
             font=("Arial",22,"bold")
         )
-        competitiontitle = ctk.CTkLabel(
+        self.competitiontitle = ctk.CTkLabel(
             self.sidebar,
             text="Competition",
         )
-        competitiondropdown = ctk.CTkOptionMenu(
+        self.competitiondropdown = ctk.CTkOptionMenu(
             self.sidebar,
             values = competition_names
         )
+
+        self.title.pack(pady=20)
+        self.competitiontitle.pack(pady=20)
+        self.competitiondropdown.pack(pady=10,padx=20)
+
         
 
     def create_mainview(self):
+        pass
 
     def create_formation(self):
+        pass
